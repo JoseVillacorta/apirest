75c197d100c9e77528362dc0a7a6572ecb6029a5
Jose Villacorta <villacortafur@gmail.com>
2025-04-14 11:03:35 -0500

    Se agrego Controller, Entities y Repositories

---
+++ src/main/java/com/apirest/apirest/Repositories/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.apirest.apirest.Repositories;
+
+import com.apirest.apirest.Entities.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+}
